fe5757dc6aaffa8b8e6cdf6322be235ae804af0b
Crowdbotics <engineering@crowdbotics.com>
2023-07-06 06:35:23 +0000

    Initial commit.

---
+++ backend/sfg56_dev_98145/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sfg56_dev_98145 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sfg56_dev_98145.settings')
+
+application = get_wsgi_application()
